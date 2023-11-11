fa81bff9c152cc0d561c29fc4fbe67bbb2787d42
Hashir mv <hashmvhashmuhammed007@gmail.com>
2023-11-12 01:58:58 +0530

    made changes to chatbox

--- dist/chat.js
+++ dist/chat.js
@@ -1,5 +1,5 @@
 $("#fixed-form-container .body").hide();
 $("#fixed-form-container .button").click(function () {
-        $(this).next("#fixed-form-container div").slideToggle(400);
+        $(this).next("#fixed-form-container div").slideToggle(600);
         $(this).toggleClass("expanded");
     });

43a4f1e033ce1681adee9a84b760f4a2408c9237
Hashir mv <hashmvhashmuhammed007@gmail.com>
2023-11-17 20:02:00 +0530

    chatbot image added ,changed its popup working

--- dist/chat.js
+++ dist/chat.js
@@ -1,5 +1,16 @@
+// $("#fixed-form-container").hide();
+// $("#chat_button").click(function () {
+//         $(this).next("#fixed-form-container div").slideToggle(600);
+//         $(this).toggleClass("expanded");
+//     });
+
+
+$("#fixed-form-container").hide();
 $("#fixed-form-container .body").hide();
-$("#fixed-form-container .button").click(function () {
-        $(this).next("#fixed-form-container div").slideToggle(600);
+$("#chat_button").click(function () {
+        // $(this).next("#fixed-form-container .body").slideToggle(300);
+        // $(this).toggleClass("expanded");
+        $("#fixed-form-container").slideToggle(300);
+        $("#fixed-form-container .body").slideToggle(300);
         $(this).toggleClass("expanded");
     });
